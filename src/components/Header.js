e13a016e67eafaa6a5b51cec9883a5f200c811b8
Kha Minh Tran <tranminhkha.1003@fgmail.com>
2022-10-12 22:06:06 +0700

    fix logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,57 @@
-import { useContext, useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { LoginContext } from '../App';
+import { useContext, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { LoginContext } from "../App";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-    const { login, setLogin } = useContext(LoginContext);
+  const navigate = useNavigate();
+  const { login, setLogin } = useContext(LoginContext);
 
-    const { setLoginMount } = useContext(LoginContext);
+  const { setLoginMount } = useContext(LoginContext);
 
-    return (
-        <div className="header">
-            <div className="wp-logo">
-                <img src={require('../asset/images/Logo1.jpg')} className="logo" />
-            </div>
-            <ul className="menu">
-                <NavLink to="/home">
-                    <li className="menu-item">Trang chủ</li>
-                </NavLink>
-                {login && (
-                    <NavLink to="/updateprice">
-                        <li className="menu-item">Cập nhật giá</li>
-                    </NavLink>
-                )}
-            </ul>
-            <div className="login">
-                {!login && (
-                    <button
-                        onClick={() => {
-                            setLoginMount(true);
-                        }}
-                    >
-                        Đăng Nhập
-                    </button>
-                )}
+  return (
+    <div className="header">
+      <div className="wp-logo">
+        <img
+          src={require("../asset/images/Logo1.jpg")}
+          className="logo"
+          alt=""
+        />
+      </div>
+      <ul className="menu">
+        <NavLink to="/home">
+          <li className="menu-item">Trang chủ</li>
+        </NavLink>
+        {login && (
+          <NavLink to="/updateprice">
+            <li className="menu-item">Cập nhật giá</li>
+          </NavLink>
+        )}
+      </ul>
+      <div className="login">
+        {!login && (
+          <button
+            onClick={() => {
+              setLoginMount(true);
+            }}
+          >
+            Đăng Nhập
+          </button>
+        )}
 
-                {login && (
-                    <button
-                        onClick={() => {
-                            setLogin(false);
-                        }}
-                    >
-                        Đăng xuất
-                    </button>
-                )}
-            </div>
-        </div>
-    );
+        {login && (
+          <button
+            onClick={() => {
+              setLogin(false);
+              navigate("/home");
+            }}
+          >
+            Đăng xuất
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/page/Update.js
+++ src/page/Update.js
@@ -1,51 +1,64 @@
 function Update() {
-    return (
-        <div id="update">
-            <h1 className="header-text">Cập Nhật Giá Vàng</h1>
-            <div className="information_box">
-                <h4 className="icontent">Tên nhân viên: XXX</h4>
-                <h4 className="icontent">
-                    Khu vực:
-                    <select id="location">
-                        <option value="TPHCM">Thành Phố Hồ Chí Minh</option>
-                        <option value="CANTHO">Cần Thơ</option>
-                        <option value="HANOI">Hà Nội</option>
-                        <option value="DANANG">Đà Nẵng</option>
-                        <option value="VINHLONG">Vĩnh Long</option>
-                    </select>
-                </h4>
-                <h4 className="icontent">
-                    Ngày cập nhật: <input type="date"></input>
-                </h4>
-                <button>Tìm</button>
-            </div>
-            <table id="table">
-                <tr>
-                    <td className="title" colSpan="4">
-                        Bảng Cập Nhật Giá Vàng
-                    </td>
-                </tr>
-                <tr>
-                    <td>STT</td>
-                    <td>Loại vàng</td>
-                    <td>Giá mua vào</td>
-                    <td>Giá bán ra</td>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>SJC</td>
-                    <td>
-                        <input class="sellprice" placeholder="Giá mua" value="123"></input>
-                    </td>
-                    <td>
-                        <input class="buyprice" placeholder="Giá bán" value="123"></input>
-                    </td>
-                </tr>
+  const location = [
+    { id: "TPHCM", name: "Thành Phố Hồ Chí Minh" },
+    { id: "CANTHO", name: "Cần Thơ" },
+    { id: "HANOI", name: "Hà Nội" },
+    { id: "DANANG", name: "Đà Nẵng" },
+    { id: "VINHLONG", name: "Vĩnh Long" },
+  ];
+  return (
+    <div id="update">
+      <h1 className="header-text">Cập Nhật Giá Vàng</h1>
+      <div className="information_box">
+        <h4 className="icontent">Tên nhân viên: XXX</h4>
+        <h4 className="icontent">
+          Khu vực:
+          <select id="location">
+            {location.map((loc) => (
+              <option value={loc.id}>{loc.name}</option>
+            ))}
+          </select>
+        </h4>
+        <h4 className="icontent">
+          Ngày cập nhật: <input type="date"></input>
+        </h4>
+        <button>Tìm</button>
+      </div>
+      <table id="table">
+        <tr>
+          <td className="title" colSpan="4">
+            Bảng Cập Nhật Giá Vàng
+          </td>
+        </tr>
+        <tr>
+          <td>STT</td>
+          <td>Loại vàng</td>
+          <td>Giá mua vào</td>
+          <td>Giá bán ra</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>SJC</td>
+          <td>
+            <input
+              className="sellprice"
+              placeholder="Giá mua"
+              value="123"
+            ></input>
+          </td>
+          <td>
+            <input
+              className="buyprice"
+              placeholder="Giá bán"
+              value="123"
+            ></input>
+          </td>
+        </tr>
 
-                <button>Lưu</button>
-            </table>
-        </div>
-    );
+        <button>Lưu</button>
+      </table>
+    </div>
+  );
 }
 
 export default Update;
